d176dc8385320ee5242760e96236221ca876e787
Hamed Bahram <64147793+HamedBahram@users.noreply.github.com>
2023-06-08 09:25:42 -0400

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>NextJs 13 Starter Kit</h1>
+      </div>
+    </section>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+                md: "1.5rem",
+                lg: "2rem",
+            },
+        },
+    },
+    plugins: [],
+};
